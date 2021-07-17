239b74f0f1808dfe5888cf640e6d35b7124b72a1
Ae-Mc <ae_mc@mail.ru>
2021-07-17 23:36:47 +0300

    Fix cookies samesite error in debug

--- backend/climbing/settings.py
+++ backend/climbing/settings.py
@@ -66,7 +66,14 @@ MIDDLEWARE = [
 ]
 
 if DEBUG:
-    CORS_ALLOW_ALL_ORIGINS = True
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:8080",
+        "http://192.168.1.33",
+        "http://192.168.1.33:8080",
+        "https://climbing.ae-mc.ru",
+    ]
+    CORS_ALLOW_CREDENTIALS = True
+    SESSION_COOKIE_SAMESITE = None
 else:
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:8080",
