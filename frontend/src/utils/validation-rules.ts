f03683dff6d7b980af4c4f495fba6b2264f57ddf
Ae-Mc <ae_mc@mail.ru>
2021-09-06 22:56:45 +0300

    Create size50MB rule for files input

--- frontend/src/utils/validation-rules.ts
+++ frontend/src/utils/validation-rules.ts
@@ -4,3 +4,11 @@ import { required } from "vee-validate/dist/rules";
 extend("required", { ...required, message: "{_field_} не может быть пустым" });
 extend("requiredMany", { ...required, message: "{_field_} не могут быть пустыми" });
 extend("requiredWomanSex", { ...required, message: "{_field_} не может быть пустой" });
+
+extend(
+  "size50MB",
+  {
+    validate: (files: File[]) => files.map(x => x.size).reduce((prev, cur) => prev + cur) < 50000000,
+    message: 'Суммарный размер файлов не должен превышать 50MB'
+  }
+);
