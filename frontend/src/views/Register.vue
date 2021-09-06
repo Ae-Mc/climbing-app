98504986b1aa4dca00a5b77f9979c425c5008171
Ae-Mc <ae_mc@mail.ru>
2021-09-06 23:02:23 +0300

    Complete Registration page
    Use new text-field instead of v-text-field
    Move submit function call from v-btn to v-form
    Write registration function

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,101 +1,114 @@
 <template>
   <v-layout fill-height>
     <v-row align="center" justify="center">
-      <v-card>
-        <v-card-title>Регистрация</v-card-title>
-        <v-card-text>
-          <validation-observer v-slot="{ invalid }" ref="observer">
-            <v-form @submit.prevent="validate">
-              <validation-provider
-                v-slot="{ errors }"
-                name="Имя пользователя"
+      <validation-observer v-slot="{ invalid }" ref="observer">
+        <v-form @submit.prevent="validate" @submit="submit">
+          <v-card>
+            <v-card-title>Регистрация</v-card-title>
+            <v-card-text>
+              <text-field
+                v-model="user.name"
+                icon="mdi-account"
+                label="Имя пользователя"
                 rules="required"
-              >
-                <v-text-field
-                  v-model="username"
-                  prepend-icon="mdi-account"
-                  label="Имя пользователя"
-                  :error-messages="errors"
-                  required
-                ></v-text-field>
-              </validation-provider>
-              <validation-provider
-                v-slot="{ errors }"
-                name="Пароль"
+                required
+              />
+              <text-field
+                v-model="user.password"
+                icon="mdi-lock"
+                label="Пароль"
                 :rules="'required|passwordMatch:' + password2"
-              >
-                <v-text-field
-                  v-model="password"
-                  prepend-icon="mdi-lock"
-                  label="Пароль"
-                  :error-messages="errors"
-                  type="password"
-                  required
-                />
-              </validation-provider>
-              <validation-provider
-                v-slot="{ errors }"
-                name="Повтор пароля"
-                :rules="'required|passwordMatch:' + password"
-              >
-                <v-text-field
-                  v-model="password2"
-                  prepend-icon="mdi-lock"
-                  label="Повтор пароля"
-                  type="password"
-                  :error-messages="errors"
-                  required
-                />
-              </validation-provider>
-              <v-btn
-                @click.stop="submit"
-                type="submit"
-                class="mr-4 mt-4"
-                :disabled="invalid"
-              >
+                type="password"
+                required
+              />
+              <text-field
+                v-model="password2"
+                icon="mdi-lock"
+                label="Повтор пароля"
+                :rules="'required|passwordMatch:' + user.password"
+                type="password"
+                required
+              />
+            </v-card-text>
+            <v-card-text class="py-0">
+              <div
+                v-if="errors.length > 0"
+                class="error--text pb-4"
+                v-html="errors.join('<br>')"
+              ></div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn type="submit" :disabled="invalid">
                 Зарегестрироваться
               </v-btn>
-              <v-btn @click.stop="$router.push('/login')" class="mt-4">
-                Войти
-              </v-btn>
-            </v-form>
-          </validation-observer>
-        </v-card-text>
-      </v-card>
+              <v-btn @click.stop="$router.push('/login')"> Войти </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </validation-observer>
     </v-row>
   </v-layout>
 </template>
-<script>
+<script lang="ts">
 import Vue from "vue";
-import { extend, ValidationProvider, ValidationObserver } from "vee-validate";
+import { extend, ValidationObserver } from "vee-validate";
 import { is, required } from "vee-validate/dist/rules";
+import TextField from "@/components/FormFields/TextField.vue";
+import { auth } from "@/store/modules/auth";
+import User from "@/models/user";
+import "@/utils/validation-rules";
 
-extend("required", { ...required, message: "{_field_} не может быть пустым" });
 extend("passwordMatch", {
   ...is,
   message: "Пароли не совпадают"
 });
 
 export default Vue.extend({
-  data: () => ({
-    username: "",
-    email: "",
-    password: "",
-    password2: ""
-  }),
+  data() {
+    return {
+      user: new User(),
+      password2: "",
+      errors: [] as string[]
+    };
+  },
   computed: {},
   methods: {
     validate() {
-      console.log(this.$refs.observer.validate());
-      console.log("Validate");
+      (this.$refs.observer as InstanceType<
+        typeof ValidationObserver
+      >).validate();
     },
     submit() {
-      alert("Submit");
+      this.errors = [];
+      auth.actions
+        .register(this.user)
+        .then(() => this.$router.push({ name: "Login" }))
+        .catch(error => {
+          console.log(error);
+          console.log(error.response);
+          if (error.response) {
+            console.log(error.response.data);
+            if (error.response.status === 400) {
+              if (error.response.data.username) {
+                this.errors = this.errors.concat(error.response.data.username);
+              }
+              if (error.response.data.password) {
+                this.errors = this.errors.concat(error.response.data.password);
+              }
+            } else {
+              this.errors = [
+                `Неизвестная ошибка. Код ошибки: ${error.response.status}`
+              ];
+            }
+          } else {
+            this.errors = ["Проверьте соединение с сетью!"];
+          }
+        });
     }
   },
   components: {
-    ValidationProvider,
-    ValidationObserver
+    ValidationObserver,
+    TextField
   }
 });
 </script>
