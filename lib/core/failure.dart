c4f253d68f81b67269ce38b5532fa3dd3ea7038a
Ae-Mc <ae_mc@mail.ru>
2022-02-01 00:10:16 +0300

    feat(core): write base usecase and failure classes

---
+++ lib/core/failure.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class Failure extends Equatable {
+  final String? message;
+  const Failure(this.message);
+
+  @override
+  List<Object> get props => [];
+  @override
+  bool get stringify => true;
+}
---
+++ lib/core/usecase.dart
@@ -0,0 +1,6 @@
+import 'package:climbing_app/core/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
