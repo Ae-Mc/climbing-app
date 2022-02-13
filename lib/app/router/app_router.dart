55eb59b62abd2cd5c7db4396b326eca909986f2f
Ae-Mc <ae_mc@mail.ru>
2022-02-13 22:56:44 +0300

    feat(root): create root page
    Create root page with bottom bar and nested navigation

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:climbing_app/app/router/guards/init_guard.dart';
 import 'package:climbing_app/features/auth/presentation/pages/auth_page.dart';
+import 'package:climbing_app/features/root/presentation/pages/root_page.dart';
 import 'package:climbing_app/features/routes/presentation/pages/routes_page.dart';
 import 'package:climbing_app/features/splash/presentation/pages/splash_page.dart';
 export 'app_router.gr.dart';
@@ -8,8 +9,15 @@ export 'app_router.gr.dart';
 @AdaptiveAutoRouter(
   replaceInRouteName: "Page,Route",
   routes: [
-    AutoRoute(guards: [InitGuard], page: AuthPage),
-    AutoRoute(guards: [InitGuard], initial: true, page: RoutesPage),
+    AutoRoute(
+      guards: [InitGuard],
+      page: RootPage,
+      initial: true,
+      children: [
+        AutoRoute(guards: [InitGuard], page: AuthPage),
+        AutoRoute(guards: [InitGuard], initial: true, page: RoutesPage),
+      ],
+    ),
     AutoRoute(page: SplashPage),
   ],
 )
---
+++ lib/features/root/presentation/pages/root_page.dart
@@ -0,0 +1,53 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:climbing_app/app/router/app_router.dart';
+import 'package:climbing_app/app/theme/bloc/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class RootPage extends StatelessWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoTabsScaffold(
+      routes: const [
+        RoutesRoute(),
+        AuthRoute(),
+      ],
+      bottomNavigationBuilder: (context, tabsRouter) {
+        return Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 8,
+                color: AppTheme.of(context).colorTheme.onBackground,
+                spreadRadius: -2,
+              ),
+            ],
+            color: AppTheme.of(context).colorTheme.background,
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.transparent,
+            currentIndex: tabsRouter.activeIndex,
+            elevation: 0,
+            iconSize: 32,
+            onTap: tabsRouter.setActiveIndex,
+            selectedItemColor: AppTheme.of(context).colorTheme.primary,
+            unselectedItemColor:
+                AppTheme.of(context).colorTheme.unselectedAppBar,
+            items: const [
+              BottomNavigationBarItem(
+                label: "Трассы",
+                icon: Icon(Icons.ballot_outlined),
+              ),
+              BottomNavigationBarItem(
+                label: "Профиль",
+                icon: Icon(Icons.person),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
