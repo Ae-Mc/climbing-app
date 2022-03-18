a3cc44502fd37dbe61b69f682dec92f6198fc87b
Ae-Mc <ae_mc@mail.ru>
2022-03-18 23:59:02 +0300

    feat(user/data): add getCurrentUser and logout methods to UserRemoteDatasource

--- lib/features/user/data/datasources/user_remote_datasource.dart
+++ lib/features/user/data/datasources/user_remote_datasource.dart
@@ -1,5 +1,8 @@
 import 'package:climbing_app/features/user/data/models/access_token.dart';
+import 'package:climbing_app/features/user/domain/entities/user.dart';
 
 abstract class UserRemoteDatasource {
+  Future<User> getCurrentUser();
   Future<AccessToken> login(String usernameOrEmail, String password);
+  Future<void> logout();
 }
--- lib/features/user/data/datasources/user_remote_datasource_impl.dart
+++ lib/features/user/data/datasources/user_remote_datasource_impl.dart
@@ -1,6 +1,7 @@
 import 'package:climbing_app/core/constants.dart';
 import 'package:climbing_app/features/user/data/datasources/user_remote_datasource.dart';
 import 'package:climbing_app/features/user/data/models/access_token.dart';
+import 'package:climbing_app/features/user/domain/entities/user.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/http.dart';
@@ -14,9 +15,14 @@ class UserRemoteDatasourceImpl implements UserRemoteDatasource {
   UserRemoteDatasourceImpl(this.authApi);
 
   @override
-  Future<AccessToken> login(String usernameOrEmail, String password) async {
-    return await authApi.login(usernameOrEmail, password);
-  }
+  Future<User> getCurrentUser() => authApi.getCurrentUser();
+
+  @override
+  Future<AccessToken> login(String usernameOrEmail, String password) =>
+      authApi.login(usernameOrEmail, password);
+
+  @override
+  Future<void> logout() => authApi.logout();
 }
 
 @singleton
@@ -25,7 +31,13 @@ abstract class AuthApi {
   @factoryMethod
   factory AuthApi(Dio dio) => _AuthApi(dio);
 
+  @GET("users/me")
+  Future<User> getCurrentUser();
+
   @POST("auth/login")
   @FormUrlEncoded()
   Future<AccessToken> login(@Field() String username, @Field() String password);
+
+  @POST("auth/logout")
+  Future<void> logout();
 }
