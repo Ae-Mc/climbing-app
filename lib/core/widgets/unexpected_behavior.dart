b2d0c604749c17685b06bc5946b7503da58222c9
Ae-Mc <ae_mc@mail.ru>
2022-03-23 21:04:57 +0300

    feat(core/UnexpectedBehavior): create UnexpectedBehavior widget

---
+++ lib/core/widgets/unexpected_behavior.dart
@@ -0,0 +1,24 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:climbing_app/app/theme/bloc/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class UnexpectedBehavior extends StatelessWidget {
+  const UnexpectedBehavior({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final colorTheme = AppTheme.of(context).colorTheme;
+    final textTheme = AppTheme.of(context).textTheme;
+
+    return Container(
+      decoration:
+          BoxDecoration(color: colorTheme.error, shape: BoxShape.circle),
+      padding: const Pad(all: 16),
+      child: Text(
+        "Неожиданное поведение. Свяжитесь с разработчиком! (Он обещал шоколадку, отговоркам не верьте!)",
+        style: textTheme.title.copyWith(color: colorTheme.onError),
+        maxLines: 8,
+      ),
+    );
+  }
+}
