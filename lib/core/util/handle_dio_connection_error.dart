1fcdf2f4e0ae788a1b66068a62f75c05e250db11
Ae-Mc <ae_mc@mail.ru>
2022-03-23 20:58:02 +0300

    fix(core/util): add SocketException handling in handleDioConnectionError

--- lib/core/util/handle_dio_connection_error.dart
+++ lib/core/util/handle_dio_connection_error.dart
@@ -1,14 +1,17 @@
+import 'dart:io';
+
 import 'package:climbing_app/core/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 Either<Failure, DioError> handleDioConnectionError(DioError error) {
   if ([
-    DioErrorType.cancel,
-    DioErrorType.connectTimeout,
-    DioErrorType.receiveTimeout,
-    DioErrorType.sendTimeout,
-  ].contains(error.type)) {
+        DioErrorType.cancel,
+        DioErrorType.connectTimeout,
+        DioErrorType.receiveTimeout,
+        DioErrorType.sendTimeout,
+      ].contains(error.type) ||
+      (error.error is SocketException)) {
     return const Left(ConnectionFailure());
   }
 
