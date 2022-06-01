9559197e72f15b402ad5e4d78738d7725b7160c9
Ae-Mc <ae_mc@mail.ru>
2022-06-01 16:19:12 +0300

    feat(core/StyledTextField): add errorText, isError and textAlign parameters

--- lib/core/widgets/styled_text_field.dart
+++ lib/core/widgets/styled_text_field.dart
@@ -4,53 +4,79 @@ import 'package:flutter/material.dart';
 
 class StyledTextField extends StatelessWidget {
   final TextEditingController? controller;
+  final String? errorText;
   final String hintText;
   final TextInputAction inputAction;
+  final bool isError;
   final TextInputType keyboardType;
   final int maxLines;
   final bool obscureText;
   final void Function()? onTap;
   final bool readOnly;
   final Widget? suffixIcon;
+  final TextAlign textAlign;
 
   const StyledTextField({
     Key? key,
     required this.hintText,
     this.controller,
+    this.errorText,
     this.inputAction = TextInputAction.next,
+    this.isError = false,
     this.keyboardType = TextInputType.text,
     this.maxLines = 1,
     this.obscureText = false,
     this.onTap,
     this.readOnly = false,
     this.suffixIcon,
+    this.textAlign = TextAlign.start,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final colorTheme = AppTheme.of(context).colorTheme;
     final textTheme = AppTheme.of(context).textTheme;
+    final border = OutlineInputBorder(
+      borderRadius: const BorderRadius.all(Radius.circular(16)),
+      borderSide: BorderSide(
+        color: isError || errorText != null
+            ? colorTheme.error
+            : colorTheme.unselected,
+        width: isError || errorText != null ? 2 : 1,
+      ),
+    );
+    final focusedBorder = border.copyWith(
+      borderSide: BorderSide(
+        width: 2,
+        color: isError || errorText != null
+            ? colorTheme.error
+            : colorTheme.primary,
+      ),
+    );
 
     return TextField(
       controller: controller,
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(16)),
-          borderSide: BorderSide(color: colorTheme.unselected),
-        ),
+        enabledBorder: border,
+        focusedBorder: focusedBorder,
+        errorBorder: focusedBorder,
+        // ignore: no-equal-arguments
+        focusedErrorBorder: focusedBorder,
         contentPadding:
             Pad(left: 16, right: suffixIcon == null ? 16 : 0, vertical: 14.5),
-        suffixIcon: suffixIcon,
+        errorText: errorText,
         hintText: hintText,
+        suffixIcon: suffixIcon,
       ),
       keyboardType: keyboardType,
       maxLines: maxLines,
+      obscureText: obscureText,
       onTap: onTap,
       readOnly: readOnly,
       style: textTheme.body1Regular,
+      textAlign: textAlign,
       textAlignVertical: TextAlignVertical.center,
       textInputAction: inputAction,
-      obscureText: obscureText,
     );
   }
 }
