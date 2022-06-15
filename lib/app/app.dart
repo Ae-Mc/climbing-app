129bacc883437cafcf029015532785df47abdab7
Ae-Mc <ae_mc@mail.ru>
2022-06-15 23:11:41 +0300

    fix(web): fix touch gestures not working on web

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,6 +7,7 @@ import 'package:climbing_app/features/splash/presentation/bloc/splash_bloc.dart'
 import 'package:climbing_app/features/splash/presentation/bloc/splash_state.dart';
 import 'package:climbing_app/features/splash/presentation/pages/splash_page.dart';
 import 'package:climbing_app/features/user/presentation/bloc/user_bloc.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -31,86 +32,86 @@ class App extends StatelessWidget {
         ),
       ],
       child: BlocBuilder<AppThemeBloc, AppTheme>(
-        builder: (context, theme) {
-          return MaterialApp(
-            title: "Скалолазание ИТМО",
-            home: BlocBuilder<SplashBloc, SplashState>(
-              builder: (context, splashState) => splashState.maybeWhen(
-                loaded: () => Router(
-                  backButtonDispatcher: RootBackButtonDispatcher(),
-                  routerDelegate: GetIt.I<AppRouter>().delegate(),
-                  routeInformationParser:
-                      GetIt.I<AppRouter>().defaultRouteParser(),
-                  routeInformationProvider:
-                      GetIt.I<AppRouter>().routeInfoProvider(),
-                ),
-                orElse: () => const SplashPage(),
+        builder: (context, theme) => MaterialApp(
+          title: "Скалолазание ИТМО",
+          scrollBehavior: const ScrollBehavior().copyWith(
+            dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse},
+          ),
+          home: BlocBuilder<SplashBloc, SplashState>(
+            builder: (context, splashState) => splashState.maybeWhen(
+              loaded: () => Router(
+                backButtonDispatcher: RootBackButtonDispatcher(),
+                routerDelegate: GetIt.I<AppRouter>().delegate(),
+                routeInformationParser:
+                    GetIt.I<AppRouter>().defaultRouteParser(),
+                routeInformationProvider:
+                    GetIt.I<AppRouter>().routeInfoProvider(),
+              ),
+              orElse: () => const SplashPage(),
+            ),
+          ),
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            cardTheme: CardTheme(
+              elevation: 8,
+              color: theme.colorTheme.surface,
+              margin: Pad.zero,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
               ),
             ),
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              cardTheme: CardTheme(
-                elevation: 8,
-                color: theme.colorTheme.surface,
-                margin: Pad.zero,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
+            chipTheme: ChipThemeData(
+              backgroundColor: theme.colorTheme.secondary,
+              padding: const Pad(horizontal: 11, vertical: 0),
+              labelPadding: Pad.zero,
+              labelStyle: theme.textTheme.chip,
+            ),
+            colorScheme: const ColorScheme.light().copyWith(
+              background: theme.colorTheme.background,
+              brightness: theme.colorTheme.brightness,
+              onPrimary: theme.colorTheme.onPrimary,
+              primary: theme.colorTheme.primary,
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith(
+                  (states) => states.contains(MaterialState.disabled)
+                      ? theme.colorTheme.unselected
+                      : theme.colorTheme.primary,
                 ),
-              ),
-              chipTheme: ChipThemeData(
-                backgroundColor: theme.colorTheme.secondary,
-                padding: const Pad(horizontal: 11, vertical: 0),
-                labelPadding: Pad.zero,
-                labelStyle: theme.textTheme.chip,
-              ),
-              colorScheme: const ColorScheme.light().copyWith(
-                background: theme.colorTheme.background,
-                brightness: theme.colorTheme.brightness,
-                onPrimary: theme.colorTheme.onPrimary,
-                primary: theme.colorTheme.primary,
-              ),
-              elevatedButtonTheme: ElevatedButtonThemeData(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith(
-                    (states) => states.contains(MaterialState.disabled)
-                        ? theme.colorTheme.unselected
-                        : theme.colorTheme.primary,
-                  ),
-                  elevation: MaterialStateProperty.all(8),
-                  minimumSize: MaterialStateProperty.all(Size.zero),
-                  padding: MaterialStateProperty.all(
-                    const Pad(horizontal: 64, vertical: 16),
-                  ),
-                  shape: MaterialStateProperty.all(
-                    const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                    ),
-                  ),
-                  textStyle: MaterialStateProperty.all(theme.textTheme.button),
-                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                elevation: MaterialStateProperty.all(8),
+                minimumSize: MaterialStateProperty.all(Size.zero),
+                padding: MaterialStateProperty.all(
+                  const Pad(horizontal: 64, vertical: 16),
                 ),
-              ),
-              fontFamily: theme.textTheme.fontFamily,
-              iconTheme: IconThemeData(color: theme.colorTheme.primary),
-              textButtonTheme: TextButtonThemeData(
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all(
-                    theme.colorTheme.secondaryVariant,
+                shape: MaterialStateProperty.all(
+                  const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
                   ),
-                  padding: MaterialStateProperty.all(Pad.zero),
-                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  textStyle:
-                      MaterialStateProperty.all(theme.textTheme.subtitle2),
                 ),
-              ),
-              textTheme: TextTheme(
-                bodyText1: theme.textTheme.body1Regular,
-                subtitle1: theme.textTheme.subtitle1,
-                subtitle2: theme.textTheme.subtitle2,
+                textStyle: MaterialStateProperty.all(theme.textTheme.button),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
               ),
             ),
-          );
-        },
+            fontFamily: theme.textTheme.fontFamily,
+            iconTheme: IconThemeData(color: theme.colorTheme.primary),
+            textButtonTheme: TextButtonThemeData(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(
+                  theme.colorTheme.secondaryVariant,
+                ),
+                padding: MaterialStateProperty.all(Pad.zero),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                textStyle: MaterialStateProperty.all(theme.textTheme.subtitle2),
+              ),
+            ),
+            textTheme: TextTheme(
+              bodyText1: theme.textTheme.body1Regular,
+              subtitle1: theme.textTheme.subtitle1,
+              subtitle2: theme.textTheme.subtitle2,
+            ),
+          ),
+        ),
       ),
     );
   }
