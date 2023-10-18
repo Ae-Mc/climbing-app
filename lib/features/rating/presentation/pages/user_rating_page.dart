fc78842324200b8abfcd3790f094f5810de37eef
Ae-Mc <ae_mc@mail.ru>
2023-10-19 01:42:33 +0300

    feat(rating): complete ascents rating page

--- lib/features/rating/presentation/pages/user_rating_page.dart
+++ lib/features/rating/presentation/pages/user_rating_page.dart
@@ -2,7 +2,9 @@ import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:climbing_app/app/theme/bloc/app_theme.dart';
 import 'package:climbing_app/core/widgets/custom_sliver_app_bar.dart';
+import 'package:climbing_app/features/rating/domain/entities/ascent_read.dart';
 import 'package:climbing_app/features/rating/domain/entities/score.dart';
+import 'package:climbing_app/features/rating/presentation/widgets/ascent_card.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -13,7 +15,25 @@ class UserRatingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorTheme = AppTheme.of(context).colorTheme;
     final textTheme = AppTheme.of(context).textTheme;
+    final List<AscentRead> sortedAscents = List.from(score.ascents);
+    final lastCountedDate = DateTime.now()
+        .subtract(const Duration(days: 45))
+        .copyWith(
+            hour: 0, minute: 0, second: 0, microsecond: 0, millisecond: 0);
+    sortedAscents.sort((a, b) {
+      if (a.date.compareTo(lastCountedDate) < 0 ||
+          b.date.compareTo(lastCountedDate) < 0) {
+        return b.date.compareTo(a.date);
+      }
+      var result = b.route.category.compareTo(a.route.category);
+      if (result == 0) {
+        return b.date.compareTo(a.date);
+      }
+      return result;
+    });
+
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -23,25 +43,39 @@ class UserRatingPage extends StatelessWidget {
           ),
           // Slivers
           SliverPadding(
-            padding: const Pad(all: 8),
+            padding: const Pad(all: 16),
             sliver: SliverList.list(
               children: [
-                const SizedBox(height: 16),
                 if (score.participations.isNotEmpty) ...[
                   Text(
                     "Соревнования",
-                    style: textTheme.subtitle1,
+                    style: textTheme.title,
                     textAlign: TextAlign.center,
                   ),
                   const SizedBox(height: 16),
+                  const Text(
+                    "Work in progress, coming soon...",
+                    textAlign: TextAlign.center,
+                  ),
                 ],
-                if (score.ascents.isNotEmpty) ...[
+                if (sortedAscents.isNotEmpty) ...[
                   Text(
                     "Пролазы",
-                    style: textTheme.subtitle1,
+                    style: textTheme.title,
                     textAlign: TextAlign.center,
                   ),
                   const SizedBox(height: 16),
+                  ...sortedAscents.indexed.map((e) => Padding(
+                        padding: const Pad(bottom: 16),
+                        child: AscentCard(
+                            ascent: e.$2,
+                            highlightColor:
+                                e.$2.date.compareTo(lastCountedDate) >= 0
+                                    ? e.$1 < 5
+                                        ? colorTheme.ascentTop5
+                                        : colorTheme.ascentActual
+                                    : null),
+                      ))
                 ],
                 if (score.ascents.isEmpty && score.participations.isEmpty)
                   Text(
--- lib/features/rating/presentation/widgets/ascent_card.dart
+++ lib/features/rating/presentation/widgets/ascent_card.dart
@@ -1,56 +1,74 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:climbing_app/app/theme/bloc/app_theme.dart';
 import 'package:climbing_app/features/rating/domain/entities/ascent_read.dart';
 import 'package:climbing_app/features/routes/presentation/widgets/custom_network_image.dart';
 import 'package:climbing_app/generated/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AscentCard extends StatelessWidget {
   final AscentRead ascent;
+  final Color? highlightColor;
 
-  const AscentCard({super.key, required this.ascent});
+  const AscentCard({super.key, required this.ascent, this.highlightColor});
 
   @override
   Widget build(BuildContext context) {
     final textTheme = AppTheme.of(context).textTheme;
+    var shape = Theme.of(context).cardTheme.shape;
+    final highlightColor = this.highlightColor;
+
+    if (shape is OutlinedBorder && highlightColor != null) {
+      shape = shape.copyWith(side: BorderSide(color: highlightColor, width: 2));
+    }
+
     return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(16)),
-            child: ascent.route.images.isNotEmpty
-                ? CustomNetworkImage(
-                    ascent.route.images[0].url,
-                    width: 72,
-                    height: 72,
-                    fit: BoxFit.cover,
-                  )
-                : Assets.images.status404.image(
-                    width: 72,
-                    height: 72,
-                    fit: BoxFit.cover,
-                  ),
-          ),
-          const SizedBox(width: 8),
-          Expanded(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  ascent.route.name,
-                  style: textTheme.subtitle1,
-                ),
-                Text(
-                  'Категория: ${ascent.route.category}',
-                  style: textTheme.subtitle2,
-                ),
-              ],
+      shape: highlightColor == null ? null : shape,
+      child: Padding(
+        padding: const Pad(horizontal: 16, vertical: 8),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(16)),
+              child: ascent.route.images.isNotEmpty
+                  ? CustomNetworkImage(
+                      ascent.route.images[0].url,
+                      width: 72,
+                      height: 72,
+                      fit: BoxFit.cover,
+                    )
+                  : Assets.images.status404.image(
+                      width: 72,
+                      height: 72,
+                      fit: BoxFit.cover,
+                    ),
             ),
-          ),
-        ],
+            const SizedBox(width: 8),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    ascent.route.name,
+                    style: textTheme.subtitle1,
+                  ),
+                  Text(
+                    'Категория: ${ascent.route.category}',
+                    style: textTheme.subtitle2,
+                  ),
+                  Text(
+                    'Дата пролаза: ${DateFormat('yyyy-MM-dd').format(ascent.date)}',
+                    style: textTheme.subtitle2,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
