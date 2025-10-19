2a684d256282009520ec4106123996097a2ce2ce
Ae-Mc <ae_mc@mail.ru>
2025-10-19 06:18:41 +0300

    fix: back not working on individual user rating page

--- lib/features/rating/presentation/pages/rating_router_page.dart
+++ lib/features/rating/presentation/pages/rating_router_page.dart
@@ -1,6 +1,12 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 
 @RoutePage()
-class RatingRouterPage extends AutoRouter {
+class RatingRouterPage extends StatelessWidget {
   const RatingRouterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AutoRouter();
+  }
 }
