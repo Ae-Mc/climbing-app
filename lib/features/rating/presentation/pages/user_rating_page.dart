ff8b70dc95f53f9858d59adf17c47f6f1d7f99e3
Ae-Mc <ae_mc@mail.ru>
2026-01-04 14:59:04 +0300

    feat: add two best ascents on user rating page

--- lib/features/rating/presentation/pages/user_rating_page.dart
+++ lib/features/rating/presentation/pages/user_rating_page.dart
@@ -1,15 +1,19 @@
+import 'dart:math';
+
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:climbing_app/app/theme/bloc/app_theme.dart';
 import 'package:climbing_app/arch/custom_toast/custom_toast.dart';
 import 'package:climbing_app/core/widgets/custom_sliver_app_bar.dart';
+import 'package:climbing_app/features/rating/domain/entities/ascent_read_rating.dart';
 import 'package:climbing_app/features/rating/domain/entities/score.dart';
 import 'package:climbing_app/features/rating/presentation/providers/user_rating.dart';
 import 'package:climbing_app/features/rating/presentation/widgets/ascent_card.dart';
 import 'package:climbing_app/features/rating/presentation/widgets/centered_text_with_button.dart';
 import 'package:climbing_app/features/rating/presentation/widgets/participation_card.dart';
+import 'package:climbing_app/features/routes/domain/entities/route.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Route;
 import 'package:get_it/get_it.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
@@ -34,64 +38,6 @@ class _UserRatingPageState extends ConsumerState<UserRatingPage> {
     final colorTheme = AppTheme.of(context).colorTheme;
     final textTheme = AppTheme.of(context).textTheme;
     final provider = userRatingProvider(widget.score.user.id);
-    final ascentsHeader = [
-      CenteredTextWithButton(
-        text: "Последние пролазы",
-        textStyle: textTheme.title,
-        button: IconButton(
-          onPressed: () => showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: const Text("О цветовой индикации"),
-              contentPadding: const Pad(all: 16),
-              actions: [
-                TextButton(
-                  onPressed: () => context.pop(),
-                  child: const Text('Понятно'),
-                ),
-              ],
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text.rich(
-                    TextSpan(children: [
-                      WidgetSpan(
-                        child: Box(
-                          color: colorTheme.ascentTop5,
-                          width: textTheme.body2Regular.fontSize,
-                          height: textTheme.body2Regular.fontSize,
-                        ),
-                      ),
-                      const TextSpan(
-                        text:
-                            ' рамкой обведены актуальные пролазы, используемые при подсчёте рейтинга\n',
-                      ),
-                      WidgetSpan(
-                        child: Box(
-                          color: colorTheme.ascentActual,
-                          width: textTheme.body2Regular.fontSize,
-                          height: textTheme.body2Regular.fontSize,
-                        ),
-                      ),
-                      const TextSpan(
-                        text:
-                            ' рамкой обведены актуальные пролазы, не используемые при подсчёте рейтинга из-за наличия лучших пролазов',
-                      ),
-                    ]),
-                    style: textTheme.body2Regular,
-                    maxLines: 15,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          icon: const Icon(Icons.help_outline),
-        ),
-      ),
-      const SizedBox(height: 16),
-    ];
 
     final ascents = ref.watch(provider);
     ref.listen(
@@ -114,6 +60,25 @@ class _UserRatingPageState extends ConsumerState<UserRatingPage> {
         }
       },
     );
+    final bestOfAllTime = ascents.whenData((ascents) {
+      final ascentsCopy = List<AscentReadRating>.from(ascents);
+      final bestOfAllTime = <Route, AscentReadRating>{};
+      for (final ascent in ascentsCopy) {
+        final existing = bestOfAllTime[ascent.route];
+        if (existing == null || ascent.date.isAfter(existing.date)) {
+          bestOfAllTime[ascent.route] = ascent;
+        }
+      }
+      return bestOfAllTime.values.toList()
+        ..sort((a, b) {
+          switch (b.route.category.compareTo(a.route.category)) {
+            case final categoryComparison when categoryComparison != 0:
+              return categoryComparison;
+            default:
+              return b.date.compareTo(a.date);
+          }
+        });
+    });
 
     return Scaffold(
       body: NestedScrollView(
@@ -157,7 +122,30 @@ class _UserRatingPageState extends ConsumerState<UserRatingPage> {
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          if (ascents.isNotEmpty) ...ascentsHeader,
+                          if (bestOfAllTime.requireValue.isNotEmpty) ...[
+                            Text(
+                              "Лучшие пролазы за всё время",
+                              style: textTheme.title,
+                              textAlign: TextAlign.center,
+                            ),
+                            const Box.gap(16),
+                            ...bestOfAllTime.requireValue
+                                .sublist(0,
+                                    min(bestOfAllTime.requireValue.length, 2))
+                                .map(
+                                  (e) => Padding(
+                                    padding: const Pad(bottom: 16),
+                                    child: AscentCard(
+                                      ascent: e,
+                                      highlightColor: colorTheme.gold,
+                                    ),
+                                  ),
+                                ),
+                          ],
+                          if (ascents.isNotEmpty) ...const [
+                            AscentsHeader(),
+                            Box.gap(16),
+                          ],
                           ...ascents.asMap().entries.map((e) => Padding(
                                 padding: const Pad(bottom: 16),
                                 child: AscentCard(
@@ -175,7 +163,7 @@ class _UserRatingPageState extends ConsumerState<UserRatingPage> {
                       error: (error, stackTrace) {
                         return Column(
                           children: [
-                            ...ascentsHeader,
+                            ...const [AscentsHeader(), Box.gap(16)],
                             Center(
                               child: FloatingActionButton(
                                 onPressed: () async => ref.invalidate(provider),
@@ -204,6 +192,72 @@ class _UserRatingPageState extends ConsumerState<UserRatingPage> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class AscentsHeader extends StatelessWidget {
+  const AscentsHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorTheme = AppTheme.of(context).colorTheme;
+    final textTheme = AppTheme.of(context).textTheme;
+
+    return CenteredTextWithButton(
+      text: "Последние пролазы",
+      textStyle: textTheme.title,
+      button: IconButton(
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text("О цветовой индикации"),
+            contentPadding: const Pad(all: 16),
+            actions: [
+              TextButton(
+                onPressed: () => context.pop(),
+                child: const Text('Понятно'),
+              ),
+            ],
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text.rich(
+                  TextSpan(children: [
+                    WidgetSpan(
+                      child: Box(
+                        color: colorTheme.ascentTop5,
+                        width: textTheme.body2Regular.fontSize,
+                        height: textTheme.body2Regular.fontSize,
+                      ),
+                    ),
+                    const TextSpan(
+                      text:
+                          ' рамкой обведены актуальные пролазы, используемые при подсчёте рейтинга\n',
+                    ),
+                    WidgetSpan(
+                      child: Box(
+                        color: colorTheme.ascentActual,
+                        width: textTheme.body2Regular.fontSize,
+                        height: textTheme.body2Regular.fontSize,
+                      ),
+                    ),
+                    const TextSpan(
+                      text:
+                          ' рамкой обведены актуальные пролазы, не используемые при подсчёте рейтинга из-за наличия лучших пролазов',
+                    ),
+                  ]),
+                  style: textTheme.body2Regular,
+                  maxLines: 15,
+                ),
+              ],
+            ),
+          ),
+        ),
+        icon: const Icon(Icons.help_outline),
       ),
     );
   }
